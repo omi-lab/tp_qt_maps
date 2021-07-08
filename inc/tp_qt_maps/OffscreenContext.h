1fd2f67e6a83c8226f30d70cb5c30ea856837415
Tom Paynter <tompaynter@tdpe.co.uk>
2021-07-08 13:18:14 +0100

    Fixed the OBJ import, added a plane to material import, lots of linting.

--- inc/tp_qt_maps/OffscreenContext.h
+++ inc/tp_qt_maps/OffscreenContext.h
@@ -1,12 +1,15 @@
 #ifndef tp_qt_maps_OffscreenContext_h
 #define tp_qt_maps_OffscreenContext_h
 
+#include "tp_qt_maps/Globals.h"
+
 namespace tp_qt_maps
 {
 
 //##################################################################################################
 class OffscreenContext
 {
+  TP_NONCOPYABLE(OffscreenContext);
 public:
   //################################################################################################
   OffscreenContext();
